ea71839906d834188b5ae3aa56afafb81fe25fa6
Alex Kovacs <alexdemaster@proton.me>
2024-11-20 13:45:23 +0000

    add a bash script to start compose files at the same time

---
+++ files/docker/home-server/start_all.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+docker network create home || true
+echo "Starting Media stack..."
+docker compose -f media_compose.yml up -d
+echo "Starting Utilities stack..."
+docker compose -f utilities_compose.yml up -d
